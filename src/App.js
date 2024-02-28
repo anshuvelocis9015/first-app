ba0de4b1f61cd30b5ca0a9cfbef7a4fef9765258
Anshu Kumar <anshu.kumar@velocis.in>
2024-02-28 11:01:41 +0530

    adding material ui in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ActionAreaCard from './ActionAreaCard';
 
 function App() {
   return (
     <div className="App">
       <h1>My fist React Project</h1>
+      <ActionAreaCard />
     </div>
   );
 }
